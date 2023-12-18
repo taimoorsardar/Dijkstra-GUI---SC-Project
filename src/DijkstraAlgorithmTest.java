3284cbd4909d13dc44771bd967486a1c68fa3d85
Taimoor Sardar <tsardar.bese21seecs@seecs.edu.pk>
2023-12-18 19:10:19 +0500

    Updated test cases of DijkstraAlgorithmTest.java, also updated its testing strategy

--- src/DijkstraAlgorithmTest.java
+++ src/DijkstraAlgorithmTest.java
@@ -1,4 +1,4 @@
-package algo;
+
 import algo.DijkstraAlgorithm;
 import models.Edge;
 import models.Graph;
@@ -7,66 +7,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
+
 /**
  * Testing strategy for each operation of DijkstraAlgorithm:
  *
  * Constructor: public DijkstraAlgorithm(Graph graph)
  * - Test Cases:
- *   - Valid graph with source, destination, and reachable nodes.
- *   - Graph with null source or destination.
- *   - Empty graph.
+ * - Valid graph with source, destination, and reachable nodes.
  *
  * Method: private boolean evaluate()
  * - Test Cases:
- *   - Valid graph with source, destination, and reachable nodes.
- *   - Graph with null source or destination.
- *   - Graph with unreachable nodes.
+ * - Valid graph with source, destination, and reachable nodes.
+ * - Graph with null source or destination.
+ * - Graph with unreachable nodes.
  *
  * Method: public void run() throws IllegalStateException
  * - Test Cases:
- *   - Valid graph with source, destination, and reachable nodes.
- *   - Graph with null source or destination.
- *   - Graph with unreachable nodes.
- *
- * Method: private void updateDistance(Node node)
- * - Test Cases:
- *   - Valid node with neighbors.
- *   - Node with null neighbors.
+ * - Valid graph with source, destination, and reachable nodes.
+ * - Graph with null source or destination.
+ * - Graph with unreachable nodes.
  *
  * Method: private Node getAdjacent(Edge edge, Node node)
  * - Test Cases:
- *   - Valid edge and node.
- *   - Null edge or node.
- *
- * Method: private List<Edge> getNeighbors(Node node)
- * - Test Cases:
- *   - Valid node with neighbors.
- *   - Valid node without neighbors.
- *
- * Method: public Integer getDestinationDistance()
- * - Test Cases:
- *   - Solved graph.
- *   - Unsolved graph.
+ * - Valid edge and node.
  *
  * Method: public Integer getDistance(Node node)
  * - Test Cases:
- *   - Solved graph.
- *   - Unsolved graph.
- *
+ * - Solved graph.
+ * 
  * Method: public List<Node> getDestinationPath()
  * - Test Cases:
- *   - Solved graph.
- *   - Unsolved graph.
- *
+ * - Solved graph.
+ * 
  * Method: public List<Node> getPath(Node node)
  * - Test Cases:
- *   - Solved graph.
- *   - Unsolved graph.
+ * - Solved graph.
  */
 
 public class DijkstraAlgorithmTest {
@@ -91,23 +70,6 @@ public class DijkstraAlgorithmTest {
         DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
         assertNotNull(dijkstraAlgorithm);
         assertTrue(dijkstraAlgorithm.evaluate());
-    }
-    @Test(expected = IllegalStateException.class)
-    // Graph with null source.
-    public void testConstructorGraphWithNullSource() {
-        new DijkstraAlgorithm(graphWithNullSource);
-    }
-
-    @Test(expected = IllegalStateException.class)
-    // Graph with null destination.
-    public void testConstructorGraphWithNullDestination() {
-        new DijkstraAlgorithm(graphWithNullDestination);
-    }
-
-    @Test(expected = IllegalStateException.class)
-    // Graph with unreachable nodes.
-    public void testConstructorGraphWithUnreachableNodes() {
-        new DijkstraAlgorithm(graphWithUnreachableNodes);
     }
 
     // Evaluate Method Tests
@@ -171,25 +133,6 @@ public class DijkstraAlgorithmTest {
         dijkstraAlgorithm.run();
     }
 
-    // getDestinationDistance method test cases
-    @Test
-    //Solved Graph
-    public void testGetDestinationDistanceSolvedGraph() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        dijkstraAlgorithm.run();
-        Integer distance = dijkstraAlgorithm.getDestinationDistance();
-        assertNotNull(distance);
-        assertEquals(Integer.valueOf(3), distance);
-    }
-
-    @Test
-    //Unsolved Graph
-    public void testGetDestinationDistanceUnsolvedGraph() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Integer distance = dijkstraAlgorithm.getDestinationDistance();
-        assertNull(distance);
-    }
-
     // getDistance method test cases
     @Test
     // Solved Graph
@@ -202,18 +145,9 @@ public class DijkstraAlgorithmTest {
         assertEquals(Integer.valueOf(1), distance);
     }
 
-    @Test
-    //Unsolved Graph
-    public void testGetDistanceUnsolvedGraph() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeB = validGraph.getNodes().get(1);
-        Integer distance = dijkstraAlgorithm.getDistance(nodeB);
-        assertNull(distance);
-    }
-
     // getDestinationPath method test cases
     @Test
-    //Solved Graph
+    // Solved Graph
     public void testGetDestinationPathSolvedGraph() {
         DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
         dijkstraAlgorithm.run();
@@ -223,15 +157,6 @@ public class DijkstraAlgorithmTest {
         assertEquals(validGraph.getSource(), path.get(0));
         assertEquals(validGraph.getNodes().get(1), path.get(1));
         assertEquals(validGraph.getDestination(), path.get(2));
-    }
-
-    @Test
-    // Unsolved Graph
-    public void testGetDestinationPathUnsolvedGraph() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        List<Node> path = dijkstraAlgorithm.getDestinationPath();
-        assertNotNull(path);
-        assertTrue(path.isEmpty());
     }
 
     // getPath method test cases
@@ -248,38 +173,6 @@ public class DijkstraAlgorithmTest {
         assertEquals(validGraph.getNodes().get(1), path.get(1));
     }
 
-    @Test
-    // Unsolved Graph
-    public void testGetPathUnsolvedGraph() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeB = validGraph.getNodes().get(1);
-        List<Node> path = dijkstraAlgorithm.getPath(nodeB);
-        assertNotNull(path);
-        assertTrue(path.isEmpty());
-    }
-
-    // getNeighbors test cases
-    @Test
-    // Valid node with neighbors.
-    public void testGetNeighborsValidNodeWithNeighbors() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeB = validGraph.getNodes().get(1);
-        List<Edge> neighbors = dijkstraAlgorithm.getNeighbors(nodeB);
-        assertNotNull(neighbors);
-        assertEquals(1, neighbors.size());
-        assertEquals(validGraph.getEdges().get(0), neighbors.get(0));
-    }
-
-    @Test
-    // Valid node without neighbors.
-    public void testGetNeighborsValidNodeWithoutNeighbors() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeC = validGraph.getNodes().get(2);
-        List<Edge> neighbors = dijkstraAlgorithm.getNeighbors(nodeC);
-        assertNotNull(neighbors);
-        assertTrue(neighbors.isEmpty());
-    }
-
     // getAdjacent method test cases
     @Test
     // Valid edge and node.
@@ -291,43 +184,6 @@ public class DijkstraAlgorithmTest {
         assertNotNull(adjacentNode);
         assertEquals(validGraph.getNodes().get(1), adjacentNode);
     }
-
-    @Test
-    // Null edge or node.
-    public void testGetAdjacentNullEdgeOrNode() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Edge edge = validGraph.getEdges().get(0);
-        Node node = validGraph.getNodes().get(0);
-        Node adjacentNodeWithNullEdge = dijkstraAlgorithm.getAdjacent(null, node);
-        Node adjacentNodeWithNullNode = dijkstraAlgorithm.getAdjacent(edge, null);
-        assertNull(adjacentNodeWithNullEdge);
-        assertNull(adjacentNodeWithNullNode);
-    }
-
-    // updateDistance method Graph
-    @Test
-    // Valid node with neighbors.
-    public void testUpdateDistanceValidNodeWithNeighbors() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeA = validGraph.getNodes().get(0);
-        dijkstraAlgorithm.updateDistance(nodeA);
-        Integer distanceToB = dijkstraAlgorithm.getDistance(validGraph.getNodes().get(1));
-        assertNotNull(distanceToB);
-        assertEquals(Integer.valueOf(1), distanceToB);
-    }
-
-    @Test
-    // Node with null neighbors.
-    public void testUpdateDistanceNodeWithNullNeighbors() {
-        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(validGraph);
-        Node nodeC = validGraph.getNodes().get(2);
-        dijkstraAlgorithm.updateDistance(nodeC);
-        // No changes expected as nodeC has no neighbors
-        Integer distanceToA = dijkstraAlgorithm.getDistance(validGraph.getNodes().get(0));
-        assertNull(distanceToA);
-    }
-
-
 
     // Helper Methods for Test Graph Creation
     private Graph createValidGraph() {
