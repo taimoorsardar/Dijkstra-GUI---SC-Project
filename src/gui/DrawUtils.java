d88048279d6b3d2fb5903ce84c615dbdb8a9cb34
MuhammedSalmanAhmed <114338554+MuhammedSalmanAhmed@users.noreply.github.com>
2023-12-19 22:21:33 +0500

    Updated DrawUtils.java after writing specifications

--- src/gui/DrawUtils.java
+++ src/gui/DrawUtils.java
@@ -8,6 +8,9 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class for drawing nodes and edges on a graphical canvas.
+ */
 public class DrawUtils {
     private static final int DEFAULT_RADIUS = 20;
     private static final int BOLD_EDGE_STROKE = 8;
@@ -24,10 +27,22 @@ public class DrawUtils {
     private static final Color PATH_COLOR = parseColor("#00BCD4");
     private static final Color TEXT_COLOR = parseColor("#cccccc");
 
+    /**
+     * Constructs a DrawUtils instance with the specified Graphics2D object.
+     *
+     * @param graphics2D The Graphics2D object to use for drawing.
+     */
     public DrawUtils(Graphics2D graphics2D) {
         g = graphics2D;
     }
 
+    /**
+     * Checks if a MouseEvent is within the bounds of a given Point.
+     *
+     * @param e The MouseEvent to check.
+     * @param p The Point to check against.
+     * @return true if the MouseEvent is within the bounds, false otherwise.
+     */
     public static boolean isWithinBounds(MouseEvent e, Point p) {
         int x = e.getX();
         int y = e.getY();
@@ -37,6 +52,13 @@ public class DrawUtils {
                (y <= boundY + DEFAULT_RADIUS && y >= boundY - DEFAULT_RADIUS);
     }
 
+    /**
+     * Checks if a MouseEvent is overlapping with a given Point.
+     *
+     * @param e The MouseEvent to check.
+     * @param p The Point to check against.
+     * @return true if the MouseEvent is overlapping, false otherwise.
+     */
     public static boolean isOverlapping(MouseEvent e, Point p) {
         int x = e.getX();
         int y = e.getY();
@@ -46,11 +68,24 @@ public class DrawUtils {
                (y <= boundY + 2.5 * DEFAULT_RADIUS && y >= boundY - 2.5 * DEFAULT_RADIUS);
     }
 
+    /**
+     * Checks if a MouseEvent is on an edge defined by two nodes.
+     *
+     * @param e    The MouseEvent to check.
+     * @param edge The Edge defining the two nodes.
+     * @return true if the MouseEvent is on the edge, false otherwise.
+     */
     public static boolean isOnEdge(MouseEvent e, Edge edge) {
         int dist = distToSegment(e.getPoint(), edge.getNodeOne().getCoord(), edge.getNodeTwo().getCoord());
         return dist < DEFAULT_RADIUS / 3;
     }
 
+    /**
+     * Parses a color represented as a hex string.
+     *
+     * @param colorStr The hex string representing the color (e.g., "#RRGGBB").
+     * @return The Color object parsed from the hex string. Returns Color.BLACK in case of parsing errors.
+     */
     public static Color parseColor(String colorStr) {
         try {
             return new Color(
@@ -63,6 +98,11 @@ public class DrawUtils {
         }
     }
 
+    /**
+     * Draws the weight of an edge at its midpoint.
+     *
+     * @param edge The edge for which to draw the weight.
+     */
     public void drawWeight(Edge edge) {
         Point from = edge.getNodeOne().getCoord();
         Point to = edge.getNodeTwo().getCoord();
@@ -73,6 +113,11 @@ public class DrawUtils {
         drawWeightText(String.valueOf(edge.getWeight()), x, y);
     }
 
+    /**
+     * Draws a path defined by a list of nodes.
+     *
+     * @param path The list of nodes defining the path.
+     */
     public void drawPath(List<Node> path) {
         if (path != null) {
             List<Edge> edges = new ArrayList<>();
@@ -86,11 +131,21 @@ public class DrawUtils {
         }
     }
 
+    /**
+     * Draws a path defined by an edge.
+     *
+     * @param edge The edge defining the path.
+     */
     public void drawPath(Edge edge) {
         g.setColor(PATH_COLOR);
         drawBoldEdge(edge);
     }
 
+    /**
+     * Draws a hovered edge with a bold stroke.
+     *
+     * @param edge The edge to draw in a hovered state.
+     */
     public void drawHoveredEdge(Edge edge) {
         g.setColor(HOVER_COLOR);
         drawBoldEdge(edge);
@@ -107,6 +162,11 @@ public class DrawUtils {
         drawCircle(x, y, BOLD_EDGE_RADIUS);
     }
 
+    /**
+     * Draws a regular edge.
+     *
+     * @param edge The edge to draw.
+     */
     public void drawEdge(Edge edge) {
         g.setColor(EDGE_COLOR);
         drawBaseEdge(edge);
@@ -120,6 +180,11 @@ public class DrawUtils {
         g.drawLine(from.x, from.y, to.x, to.y);
     }
 
+    /**
+     * Draws a halo around a node.
+     *
+     * @param node The node around which to draw the halo.
+     */
     public void drawHalo(Node node) {
         g.setColor(HOVER_COLOR);
         radius += TEXT_OFFSET;
@@ -127,10 +192,20 @@ public class DrawUtils {
         radius -= TEXT_OFFSET;
     }
 
+    /**
+     * Draws a source node with specific colors.
+     *
+     * @param node The source node to draw.
+     */
     public void drawSourceNode(Node node) {
         drawColoredNode(node, "#0356fc", "#03cffc");
     }
 
+    /**
+     * Draws a destination node with specific colors.
+     *
+     * @param node The destination node to draw.
+     */
     public void drawDestinationNode(Node node) {
         drawColoredNode(node, "#F44336", "#FFCDD2");
     }
@@ -148,6 +223,11 @@ public class DrawUtils {
         drawCentreText(String.valueOf(node.getId()), node.getX(), node.getY());
     }
 
+    /**
+     * Draws a regular node with hover effects.
+     *
+     * @param node The node to draw.
+     */
     public void drawNode(Node node) {
         g.setColor(NODE_COLOR);
         drawCircle(node.getX(), node.getY(), DEFAULT_RADIUS);
@@ -161,6 +241,13 @@ public class DrawUtils {
         drawCentreText(String.valueOf(node.getId()), node.getX(), node.getY());
     }
 
+    /**
+     * Draws text representing the weight of an edge.
+     *
+     * @param text The text to draw.
+     * @param x    The x-coordinate of the text.
+     * @param y    The y-coordinate of the text.
+     */
     public void drawWeightText(String text, int x, int y) {
         g.setColor(TEXT_COLOR);
         FontMetrics fm = g.getFontMetrics();
@@ -168,6 +255,13 @@ public class DrawUtils {
         g.drawString(text, (int) (x - t_width / 2), (y + fm.getMaxAscent() / 2));
     }
 
+    /**
+     * Draws centered text on the canvas.
+     *
+     * @param text The text to draw.
+     * @param x    The x-coordinate of the text.
+     * @param y    The y-coordinate of the text.
+     */
     public void drawCentreText(String text, int x, int y) {
         FontMetrics fm = g.getFontMetrics();
         double t_width = fm.getStringBounds(text, g).getWidth();
@@ -175,6 +269,7 @@ public class DrawUtils {
     }
 
     // Calculations
+
     private void drawCircle(int x, int y, int diameter) {
         g.fillOval(x - diameter, y - diameter, 2 * diameter, 2 * diameter);
     }
