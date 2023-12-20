0d9e756d9c62f8a81183cd0f2001a19648ff9299
MuhammedSalmanAhmed <114338554+MuhammedSalmanAhmed@users.noreply.github.com>
2023-12-20 18:40:51 +0500

    Updated Node.java by adding specifications

--- src/models/Node.java
+++ src/models/Node.java
@@ -1,56 +1,124 @@
 package models;
 
 import java.awt.*;
-import java.util.*;
 import java.util.List;
 
+/**
+ * The {@code Node} class represents a node in a graph, characterized by its unique identifier,
+ * coordinates, and an optional path.
+ */
 public class Node {
+
+    /** The coordinates of the node. */
     private Point coord = new Point();
+
+    /** The unique identifier of the node. */
     private int id;
-    private java.util.List<Node> path;
 
-    public Node(){}
+    /** The list of nodes representing a path associated with this node. */
+    private List<Node> path;
 
-    public Node(int id){
+    /**
+     * Constructs an empty {@code Node}. The coordinates and identifier need to be set separately.
+     */
+    public Node() {}
+
+    /**
+     * Constructs a {@code Node} with the specified identifier and default coordinates (0, 0).
+     *
+     * @param id The unique identifier for the node.
+     */
+    public Node(int id) {
         this.id = id;
     }
 
-    public Node(Point p){
-        this.coord = p;
+    /**
+     * Constructs a {@code Node} with the specified coordinates.
+     *
+     * @param p The coordinates of the node.
+     */
+    public Node(Point p) {
+        this.coord = new Point(p);
     }
 
-    public void setId(int id){
+    /**
+     * Sets the unique identifier for the node.
+     *
+     * @param id The unique identifier for the node.
+     */
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setCoord(int x, int y){
+    /**
+     * Sets the coordinates of the node.
+     *
+     * @param x The x-coordinate of the node.
+     * @param y The y-coordinate of the node.
+     */
+    public void setCoord(int x, int y) {
         coord.setLocation(x, y);
     }
 
-    public Point getCoord(){
+    /**
+     * Gets the coordinates of the node.
+     *
+     * @return The coordinates of the node.
+     */
+    public Point getCoord() {
         return coord;
     }
 
+    /**
+     * Sets the path associated with this node.
+     *
+     * @param path The list of nodes representing a path.
+     */
     public void setPath(List<Node> path) {
         this.path = path;
     }
 
+    /**
+     * Gets the path associated with this node.
+     *
+     * @return The list of nodes representing a path.
+     */
     public List<Node> getPath() {
         return path;
     }
 
-    public int getX(){
+    /**
+     * Gets the x-coordinate of the node.
+     *
+     * @return The x-coordinate of the node.
+     */
+    public int getX() {
         return (int) coord.getX();
     }
 
-    public int getY(){
+    /**
+     * Gets the y-coordinate of the node.
+     *
+     * @return The y-coordinate of the node.
+     */
+    public int getY() {
         return (int) coord.getY();
     }
 
-    public int getId(){
+    /**
+     * Gets the unique identifier of the node.
+     *
+     * @return The unique identifier of the node.
+     */
+    public int getId() {
         return id;
     }
 
+    /**
+     * Returns a string representation of the node.
+     *
+     * @return A string representation of the node, including its identifier.
+     */
     @Override
     public String toString() {
         return "Node " + id;
