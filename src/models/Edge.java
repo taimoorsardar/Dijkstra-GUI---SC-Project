b1c23082a226634f3aa2c18396a0232c68a75d64
IzzahAslam <114334813+IzzahAslam@users.noreply.github.com>
2023-12-20 06:30:15 -0800

    Update Edge.java
    After Code review and specifications

--- src/models/Edge.java
+++ src/models/Edge.java
@@ -1,43 +1,92 @@
+Edge class:
+
 package models;
 
+/**
+ * Represents an edge connecting two nodes in a graph.
+ */
 public class Edge {
-    private Node one;
-    private Node two;
-    private int weight = 1;
+    private static final int DEFAULT_WEIGHT = 1;
 
-    public Edge(Node one, Node two){
-        this.one = one;
-        this.two = two;
+    private Node startNode;
+    private Node endNode;
+    private int weight = DEFAULT_WEIGHT;
+
+    /**
+     * Constructs an Edge object with two connected nodes.
+     *
+     * @param startNode The starting node of the edge.
+     * @param endNode   The ending node of the edge.
+     */
+    public Edge(Node startNode, Node endNode) {
+        this.startNode = startNode;
+        this.endNode = endNode;
     }
 
-    public Node getNodeOne(){
-        return one;
+    /**
+     * Gets the starting node of the edge.
+     *
+     * @return The starting node.
+     */
+    public Node getStartNode() {
+        return startNode;
     }
 
-    public Node getNodeTwo(){
-        return two;
+    /**
+     * Gets the ending node of the edge.
+     *
+     * @return The ending node.
+     */
+    public Node getEndNode() {
+        return endNode;
     }
 
-    public void setWeight(int weight){
+    /**
+     * Sets the weight of the edge.
+     *
+     * @param weight The new weight value.
+     */
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    public int getWeight(){
+    /**
+     * Gets the weight of the edge.
+     *
+     * @return The weight value.
+     */
+    public int getWeight() {
         return weight;
     }
 
-    public boolean hasNode(Node node){
-        return one==node || two==node;
+    /**
+     * Checks if the given node is part of the edge.
+     *
+     * @param node The node to check.
+     * @return True if the node is part of the edge, false otherwise.
+     */
+    public boolean containsNode(Node node) {
+        return startNode == node || endNode == node;
     }
 
+    /**
+     * Checks if the given edge is equal to this edge.
+     *
+     * @param edge The edge to compare.
+     * @return True if the edges are equal, false otherwise.
+     */
     public boolean equals(Edge edge) {
-        return (one ==edge.one && two ==edge.two) || (one ==edge.two && two ==edge.one) ;
+        return (startNode == edge.startNode && endNode == edge.endNode) ||
+                (startNode == edge.endNode && endNode == edge.startNode);
     }
 
+    /**
+     * Returns a string representation of the edge.
+     *
+     * @return String representation of the edge.
+     */
     @Override
     public String toString() {
-        return "Edge ~ "
-                + getNodeOne().getId() + " - "
-                + getNodeTwo().getId();
+        return "Edge ~ " + getStartNode().getId() + " - " + getEndNode().getId();
     }
 }
